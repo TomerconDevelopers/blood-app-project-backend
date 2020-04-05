f2718701ee42de467ae6bff5d2ff347ff2952131
Ajay Prakash Nair <42165927+ajaynair710@users.noreply.github.com>
2020-04-05 13:57:31 +0530

    Update UserProfile.php

--- UserProfile.php
+++ UserProfile.php
@@ -1,25 +1,99 @@
-<?php
-
-require_once('connection.php');	
-$json = file_get_contents('php://input');
- 
-// Decode the received JSON and Store into $obj variable.
-$obj = json_decode($json,true);
-//take the username and password from the app
-$uname=$obj["uname"];
-$password=$obj["pass"];
-//sql query to fetch the details of the user
-$CheckSQL4 = "SELECT * FROM users WHERE username='$uname'";
-//fetches the details of the user
-$check4 = mysqli_fetch_array(mysqli_query($con,$CheckSQL4));
-//if success,the details is returned
-if(isset($check4)){
-    //the details are encoded to json and echoed
-     echo json_encode($check4);
-}
-else{
-    echo "Try again";
-}
-
-
+<?php 
+    session_start();
+    $current_user = $_SESSION['user_username'];
+    $user_username = mysqli_real_escape_string($database,$_REQUEST['user_username']);
+    $profile_username=$rws['user_username'];
 ?>
+<?php
+    $url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+?>
+<div class="profile">
+	<div class="row clearfix">
+		<!-- <div class="col-md-12 column"> -->
+            <div>
+                <center>
+                    <img src="userfiles/avatars/<?php echo $rws['user_avatar'];?>" class="img-responsive profile-avatar">
+                </center>
+                <h1 class="text-center profile-text profile-name"><?php echo $rws['user_firstname'];?> <?php echo $rws['user_lastname'];?></h1>
+                <h2 class="text-center profile-text profile-profession"><?php echo $rws['user_profession'];?></h2>
+                <br>
+                <div class="panel-group white" id="panel-profile">
+                    <div class="panel panel-default">
+                        <div id="panel-element-info" class="panel-collapse collapse in">
+                            <div class="panel-body">
+                                <div class="col-md-8 column">
+                                    <p class="text-center profile-title"><i class="fa fa-info"></i> Basic</p>
+                                    <hr>
+<?php
+    if ($rws['user_shortbio']){
+?>   
+                                    <div class="col-md-4">
+                                        <p class="profile-details"><i class="fa fa-info"></i> Bio</p>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <p><?php echo $rws['user_shortbio'];?></p>
+                                    </div>
+<?php } ?>
+<?php
+    if ($rws['user_BloodGroup']){
+?>   
+                                    <div class="col-md-4">
+                                        <p class="profile-details"><i class="fa fa-info"></i> Blood Group</p>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <p><?php echo $rws['user_BloodGroup'];?></p>
+                                    </div>
+<?php } ?>
+<?php
+    if ($rws['user_address']){
+?>   
+                                    <div class="col-md-4">
+                                        <p class="profile-details"><i class="fa fa-info"></i> Location</p>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <p><?php echo $rws['user_address'];?></p>
+                                    </div>
+<?php } ?>
+<?php
+    if ($rws['user_email']){
+?>   
+                                    <div class="col-md-4">
+                                        <p class="profile-details"><i class="fa fa-envelope"></i> Email</p>
+                                    </div>
+                                    <div class="col-md-8">                                    
+                                        <p><?php echo $rws['user_email'];?></p>
+                                    </div>
+<?php } ?>
+
+                                </div>
+                                <div class="col-md-4 column">
+                                   
+<?php
+    if ($rws['user_gender']){
+?>   
+                                    <div class="col-md-4">
+                                        <p class="profile-details"><i class="fa fa-user"></i> Gender</p>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <p><?php echo $rws['user_gender'];?></p>
+                                    </div>
+<?php } ?>
+<?php
+    if ($rws['user_dob']){
+?>   
+                                    <div class="col-md-4">
+                                        <p class="profile-details"><i class="fa fa-calendar"></i> Date of Birth</p>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <p><?php echo $rws['user_dob'];?></p>
+                                    </div>
+<?php } ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+		<!-- </div> -->
+	</div>
+</div>
