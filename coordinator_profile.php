de418600f7931745f2a683f4e22952aedba36d28
sandrasmenon18 <40963950+sandrasmenon18@users.noreply.github.com>
2020-04-09 13:40:30 +0530

    coordinator profile

---
+++ coordinator_profile.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once('connection.php');	
+$json = file_get_contents('php://input');
+ 
+// Decode the received JSON and Store into $obj variable.
+$obj = json_decode($json,true);
+
+//take the username and password from the app
+$uid=$obj["userid"];
+$password=$obj["pass"];
+
+//sql query to fetch the details of the user
+$CheckSQL = "SELECT * FROM coordinators WHERE userid='$uid' and pass='$password'";
+
+//fetches the details of the user
+$check = mysqli_fetch_array(mysqli_query($con,$CheckSQL));
+
+
+//if success,the details is returned
+if(isset($check)){
+    //the details are encoded to json and echoed
+     echo json_encode($check);
+}
+else{
+    echo "Try again";
+}
+
+
+?>
