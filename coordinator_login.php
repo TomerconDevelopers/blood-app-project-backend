d49d86f995a8c9c0e805b003032d765f6657e462
sandrasmenon18 <40963950+sandrasmenon18@users.noreply.github.com>
2020-04-19 17:33:18 +0530

    Add files via upload

---
+++ coordinator_login.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once('connection.php');	
+$json = file_get_contents('php://input');
+ 
+// Decode the received JSON and Store into $obj variable.
+$obj = json_decode($json,true);
+//take the username and password from the app
+$uname=$obj["uname"];
+$password=$obj["pass"];
+//echo $uname;
+//sql query to fetch the details of the user
+$CheckSQL4 = "SELECT * FROM coordinators WHERE userid='$uname' and pass='$password'";
+//fetches the details of the user
+$check4 = mysqli_fetch_array(mysqli_query($con,$CheckSQL4));
+//if success,the details is returned
+if(isset($check4)){
+    //the details are encoded to json and echoed
+     echo json_encode($check4);
+}
+else{
+    echo "Invalid Username/Password";
+}
+?>
