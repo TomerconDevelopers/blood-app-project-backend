2d73dd3a11b72e746b63ad6213f19a9eac877917
sandrasmenon18 <40963950+sandrasmenon18@users.noreply.github.com>
2020-04-19 12:11:18 +0530

    Update and rename Coordinatorrequests.sql to coord_requests.sql

--- coord_requests.sql
+++ coord_requests.sql
@@ -1,4 +1,11 @@
-
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 19, 2020 at 08:39 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -18,34 +25,39 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cordinatorrequests`
+-- Table structure for table `coord_requests`
 --
 
-CREATE TABLE `cordinatorrequests` (
-  `coordinatorid` int(11) NOT NULL,
+CREATE TABLE `coord_requests` (
+  `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `age` int(11) NOT NULL,
-  `date` bigint(20) NOT NULL,
+  `date` date NOT NULL,
   `bloodgroup` enum('A+','A-','B+','B-','AB+','AB-','O+','O-') NOT NULL,
   `bloodqty` int(11) NOT NULL,
-  `bystander_name` varchar(30) NOT NULL,
-  `bystander_contacts` int(15) NOT NULL,
-  `bystander_alt_contacts` int(15) NOT NULL,
+  `bystander_contacts` varchar(15) NOT NULL,
+  `bystander_alt_contacts` varchar(15) NOT NULL,
   `district` varchar(30) NOT NULL,
   `taluk` varchar(50) NOT NULL,
   `hospital` varchar(200) NOT NULL,
-  `verified_requests` int(11) NOT NULL,
-  `status` varchar(30) NOT NULL,
+  `coor_id` varchar(15) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `verification` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `coord_requests`
+--
+
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `cordinatorrequests`
+-- Indexes for table `coord_requests`
 --
-ALTER TABLE `cordinatorrequests`
+ALTER TABLE `coord_requests`
   ADD PRIMARY KEY (`bystander_contacts`),
   ADD UNIQUE KEY `id` (`id`);
 
@@ -54,10 +66,10 @@ ALTER TABLE `cordinatorrequests`
 --
 
 --
--- AUTO_INCREMENT for table `cordinatorrequests`
+-- AUTO_INCREMENT for table `coord_requests`
 --
-ALTER TABLE `cordinatorrequests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `coord_requests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
